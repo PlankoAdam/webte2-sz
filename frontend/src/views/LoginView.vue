2e0cf24c56e457323e4390640adef9e452c09e8d
Adam Planko <adi.planko@gmail.com>
2024-05-01 16:50:51 +0200

    login styling

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <template>
   <main class="flex justify-center">
     <div class="bg-gradient-to-b from-[var(--color-bg-soft)] p-8 rounded-xl">
+      <h1 class="mb-8 text-2xl font-light uppercase">Log in</h1>
       <div class="mb-8">
         <FormKit type="form" action="">
           <FormKit type="text" label="User name"></FormKit>
           <FormKit type="password" label="Password"></FormKit>
         </FormKit>
       </div>
-      <div class="text-sm">
+      <div class="text-sm text-center">
         <span class="text-slate-500 me-2">Don't have an account?</span>
         <RouterLink to="/signup" class="hover:text-[var(--color-heading)] transition-all"
           >Sign up</RouterLink
--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="flex justify-center">
     <div class="bg-gradient-to-b from-[var(--color-bg-soft)] p-8 rounded-xl">
+      <h1 class="mb-8 text-2xl font-light uppercase">Sign up</h1>
       <div class="mb-8">
         <FormKit type="form" action="">
           <FormKit type="email" label="E-mail"></FormKit>
@@ -9,7 +10,7 @@
           <FormKit type="password" label="Confirm password"></FormKit>
         </FormKit>
       </div>
-      <div class="text-sm">
+      <div class="text-sm text-center">
         <span class="text-slate-500 me-2">Already have an account?</span>
         <RouterLink to="/login" class="hover:text-[var(--color-heading)] transition-all"
           >Log in</RouterLink
