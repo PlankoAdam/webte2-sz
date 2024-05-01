e5115b1a6d2dd47c0f6f5dfc1538da9311aea793
Adam Planko <adi.planko@gmail.com>
2024-05-01 15:38:38 +0200

    sign up page

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -3,13 +3,15 @@
     <div class="bg-gradient-to-b from-[var(--color-background-soft)] p-8 rounded-xl">
       <div class="mb-8">
         <FormKit type="form" action="">
+          <FormKit type="email" label="E-mail"></FormKit>
           <FormKit type="text" label="User name"></FormKit>
           <FormKit type="password" label="Password"></FormKit>
+          <FormKit type="password" label="Confirm password"></FormKit>
         </FormKit>
       </div>
       <div class="text-sm">
-        <span>Don't have an account?</span>
-        <RouterLink to="/signup">Sign up</RouterLink>
+        <span>Already have an account?</span>
+        <RouterLink to="/login">Log in</RouterLink>
       </div>
     </div>
   </main>
