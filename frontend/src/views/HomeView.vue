bbfbd5d9062f5915dd6c7cefdc2e3f5b085b008c
Adam Planko <adi.planko@gmail.com>
2024-05-02 21:26:03 +0200

    welcome screen

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,25 +6,38 @@ const langStore = useLanguageStore()
 </script>
 
 <template>
-  <main>
-    <FormKit
-      type="form"
-      submit-label="Enter"
-      :actions="false"
-      #default="{ state: { valid } }"
-      @submit="
-        (formData) => {
-          $router.push(`/${formData.code}`)
-        }
-      "
-    >
+  <main class="flex justify-center">
+    <div class="flex flex-col items-center">
+      <div class="mb-16">
+        <h1 class="text-9xl text-center font-light text-[var(--color-heading)] mb-4 select-none">
+          interact<span
+            class="text-[var(--ac500)] dark:text-[var(--ac400)] hover:drop-shadow-[0_0_10px_var(--ac400)] dark:hover:drop-shadow-[0_0_10px_var(--ac500)] transition-all duration-700"
+            >ED</span
+          >
+        </h1>
+        <h2 class="text-2xl text-center italic">
+          {{ langStore.t('Where every student has a voice', 'Kde má každý študent svoj hlas') }}
+        </h2>
+      </div>
       <FormKit
-        name="code"
-        :label="langStore.t('Question code', 'Kód otázky')"
-        type="text"
-        validation="required|length:5,5"
-      ></FormKit>
-      <FormKit type="submit" :disabled="!valid" input-class="m-0" />
-    </FormKit>
+        type="form"
+        submit-label="Enter"
+        :actions="false"
+        #default="{ state: { valid } }"
+        @submit="
+          (formData) => {
+            $router.push(`/${formData.code}`)
+          }
+        "
+      >
+        <FormKit
+          name="code"
+          :label="langStore.t('Question code', 'Kód otázky')"
+          type="text"
+          validation="required|length:5,5"
+        ></FormKit>
+        <FormKit label="Enter" type="submit" :disabled="!valid" input-class="m-0" />
+      </FormKit>
+    </div>
   </main>
 </template>
