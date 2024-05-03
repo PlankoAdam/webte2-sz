4fa0750dfe7027020ae6c0a7c6e891a2cd39031e
Adam Planko <adi.planko@gmail.com>
2024-05-03 13:41:39 +0200

    basic details view

--- frontend/src/views/question-views/QuestionDetailsView.vue
+++ frontend/src/views/question-views/QuestionDetailsView.vue
@@ -1,41 +1,64 @@
 <template>
+  <QRModal
+    v-if="showModal"
+    @close="showModal = false"
+    :qrsrc="data.qrsrc"
+    :code="data.code"
+  ></QRModal>
   <main
     class="mt-[var(--nav-h)] lg:mt-0 bg-[var(--color-bg)] fixed top-0 bottom-0 overflow-y-scroll lg:relative lg:ms-[28rem] z-10 w-full lg:h-full h-[100vh]"
   >
     <div class="flex flex-col items-center p-8">
-      <div class="size-96 bg-white"></div>
-      <div>{{ $route.params.id }}</div>
-      <div class="text-lg hidden">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium nulla amet recusandae
-        voluptatibus ipsa tempora fugiat facilis non dolores, facere nostrum officia obcaecati
-        voluptas voluptates ut optio officiis saepe impedit alias ad necessitatibus vel. Praesentium
-        similique, exercitationem reiciendis esse totam laborum rem fugiat dignissimos voluptates
-        inventore aut obcaecati, adipisci corrupti molestias alias ratione iure! Sed aut nisi
-        suscipit adipisci sit. Pariatur deserunt nostrum eveniet iusto sequi earum repellendus
-        doloribus, ducimus et laudantium quod hic minima tempora minus dignissimos praesentium
-        tenetur soluta, fugiat tempore maiores harum? Numquam animi iusto dignissimos quis, aliquam
-        atque quaerat ipsum praesentium eum quae iste nesciunt! Debitis quidem dolor, ex natus
-        cupiditate molestias optio dolorum nulla, nobis doloribus labore deserunt quasi vitae harum
-        modi autem aliquam sapiente unde cum. Quos qui reprehenderit impedit, inventore incidunt
-        nisi dignissimos ad eligendi sed repudiandae accusantium vero quasi voluptatum fugiat
-        officiis sint sapiente voluptatibus illo quod vitae aut, quam iure in pariatur? Placeat,
-        dolorum praesentium, dolores non fuga magnam, nisi molestias iste doloribus totam sequi cum
-        atque! Praesentium corporis inventore sit dolore, nemo expedita, itaque laudantium molestias
-        consequatur, libero mollitia ipsum tempora nobis quam nulla autem quos neque aliquid
-        nesciunt aut similique ullam voluptatum! Reiciendis dolores corrupti odit ea deserunt aut.s
-        optio dolorum nulla, nobis doloribus labore deserunt quasi vitae harum modi autem aliquam
-        sapiente unde cum. Quos qui reprehenderit impedit, inventore incidunt nisi dignissimos ad
-        eligendi sed repudiandae accusantium vero quasi voluptatum fugiat officiis sint sapiente
-        voluptatibus illo quod vitae aut, quam iure in pariatur? Placeat, dolorum praesentium,
-        dolores non fuga magnam, nisi molestias iste doloribus totam sequi cum atque! Praesentium
-        corporis inventore sit dolore, nemo expedita, itaque laudantium molestias consequatur,
-        libero mollitia ipsum tempora nobis quam nulla autem quos neque aliquid nesciunt aut
-        similique ullam voluptatum! Reiciendis dolores corrupti odit ea deserunt aut
+      <div class="flex flex-col mb-16">
+        <div
+          @click="showModal = true"
+          class="size-fit bg-white p-2 rounded-md mb-2 hover:cursor-pointer"
+        >
+          <img :src="data.qrsrc" alt="QR code" class="size-96" />
+        </div>
+        <h1 class="text-center text-4xl text-[var(--color-heading)] font-mono font-bold">
+          {{ $route.params.id }}
+        </h1>
+      </div>
+      <div class="flex flex-col">
+        <h1 class="text-6xl font-light mb-8">{{ data.question }}</h1>
+        <ul class="flex flex-col space-y-2">
+          <li
+            v-for="ans in data.answers"
+            :key="ans"
+            class="p-1 px-4 rounded-sm bg-[var(--color-bg-soft)]"
+          >
+            {{ ans }}
+          </li>
+        </ul>
       </div>
     </div>
   </main>
 </template>
 
 <script setup>
+import QRModal from '@/components/QRModal.vue'
+import { watch } from 'vue'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 // TODO get data from server
+
+const route = useRoute()
+
+const dummyData = {
+  code: route.params.id,
+  question: 'Lorem ipsum?',
+  qrsrc: 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example',
+  active: true,
+  answers: ['ans1', 'ans2', 'ans3']
+}
+
+// TODO update data on route change
+watch(
+  () => route.params.id,
+  (newId) => (dummyData.code = newId)
+)
+
+const data = ref(dummyData)
+const showModal = ref(false)
 </script>
