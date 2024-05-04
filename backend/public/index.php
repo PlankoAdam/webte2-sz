2f9a3c72232dfdb56a13242a2942f22b0fb601cd
Furcika <jurik.florian@gmail.com>
2024-05-05 00:56:11 +0200

    answer GET,POST +  database

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,6 +4,10 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set('Europe/Bratislava');
+
+
 $app = AppFactory::create();
 
 // Database connection parameters
@@ -112,5 +116,47 @@ $app->delete('/users/{id}', function (Request $request, Response $response, $arg
         ->withHeader('Content-Type', 'application/json')
         ->withStatus(200);
 });
+
+// GET route to retrieve all answers
+$app->get('/answer', function (Request $request, Response $response) use ($pdo) {
+    $sql = "SELECT * FROM answers";
+    $stmt = $pdo->query($sql);
+    $answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Return response as JSON
+    $response->getBody()->write(json_encode($answers));
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(200);
+});
+
+// POST route to create a new answer
+$app->post('/answer', function (Request $request, Response $response) use ($pdo) {
+    $body = $request->getBody()->getContents();
+    // Decode the JSON data into an associative array
+    $data = json_decode($body, true);
+
+    // Automatically set the current date and time
+    $data['date_created'] = date('Y-m-d H:i:s');
+
+    $sql = "INSERT INTO answers (code, answer, date_created) VALUES (:code, :answer, :date_created)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':code' => $data['code'],
+        ':answer' => $data['answer'],
+        ':date_created' => $data['date_created']
+    ]);
+
+    // Encode the response data as JSON
+    $response->getBody()->write(json_encode(['message' => 'Answer created']));
+
+    // Set the response headers and status code
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(200);
+});
+
+
+
 
 $app->run();
