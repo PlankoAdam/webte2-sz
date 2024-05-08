c0638fb451264ff8175112783a3f45cdb2562e3a
amj-j <adam.janos24@gmail.com>
2024-05-08 14:35:30 +0200

    Added endpoint handler method for changing users password into authentication.php

--- backend/public/authentication.php
+++ backend/public/authentication.php
@@ -13,7 +13,7 @@ function getErrorResponse(Response $response, int $code, string $message) {
 
 
 // POST route to sing up (obtain a JWT token)
-$app->post('/login', function (Request $request, Response $response) use ($pdo, $secretKey) {
+$app->post('/user/login', function (Request $request, Response $response) use ($pdo, $secretKey) {
     $body = $request->getBody()->getContents();
     $data = json_decode($body, true);
 
@@ -52,7 +52,7 @@ $app->post('/login', function (Request $request, Response $response) use ($pdo, 
 
 
 // POST route to register a new user
-$app->post('/register', function (Request $request, Response $response) use ($pdo) {
+$app->post('/user/register', function (Request $request, Response $response) use ($pdo) {
     $body = $request->getBody()->getContents();
     $data = json_decode($body, true);
 
@@ -86,3 +86,43 @@ $app->post('/register', function (Request $request, Response $response) use ($pd
     }
 });
 
+
+// PUT route to change user's password
+$app->put('/user/password', function (Request $request, Response $response) use ($pdo) {
+    $body = $request->getBody()->getContents();
+    $data = json_decode($body, true);
+
+    if (!isset($data['old_password']) || !isset($data['new_password'])) {
+        return getErrorResponse($response, 400, 'The format of the request body is invalid.');
+    }
+
+    try {
+        $stmt = $pdo->prepare("SELECT password FROM users WHERE id = :id");
+        $stmt->bindParam(":id", $request->getAttribute('user_id'), PDO::PARAM_STR);
+        $stmt->execute();
+        if ($stmt->rowCount() == 1) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $hashed_old_password = $row['password'];
+
+            if (password_verify($data['old_password'], $hashed_old_password)) {
+                $hashed_new_password = password_hash($data['new_password'], PASSWORD_ARGON2ID);
+
+                $stmt = $pdo->prepare("UPDATE users SET password = :password WHERE id = :id");
+                $stmt->bindParam(":password", $hashed_new_password, PDO::PARAM_STR);
+                $stmt->bindParam(":id", $request->getAttribute('user_id'), PDO::PARAM_STR);
+                $stmt->execute();
+
+                $response->getBody()->write(json_encode(['message' => "Password updated successfully."]));
+                return $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+            } else {
+                return getErrorResponse($response, 400, 'Incorrect old password.');
+            }
+        } else {
+            return getErrorResponse($response, 500, 'Internal server error.');
+        }
+    } catch (PDOException $e) {
+        return getErrorResponse($response, 500, 'Database error.');
+    }
+})->add(new JWTAuthMiddleware());
