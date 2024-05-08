bf48df148504f09d37110c7e85172c0dd825fdba
amj-j <adam.janos24@gmail.com>
2024-05-08 16:58:27 +0200

    backend file tree reorganized

--- backend/public/index.php
+++ backend/public/index.php
@@ -9,23 +9,18 @@ date_default_timezone_set('Europe/Bratislava');
 
 $secretKey = $_ENV['SECRET_JWT_KEY'];
 
-require __DIR__ . '/auth_middleware.php';
+require __DIR__ . '/../db/connection.php';
+require __DIR__ . '/../middleware/auth_middleware.php';
 
 JWTAuthMiddleware::$secretKey = $secretKey;
 
 
 $app = AppFactory::create();
 
-require __DIR__ . '/connection.php';
-
-require __DIR__ . '/users.php';
-
-require __DIR__ . '/answers.php';
-
-require __DIR__ . '/questions.php';
-
-require __DIR__ . '/subjects.php';
-
-require __DIR__ . '/authentication.php';
+require __DIR__ . '/../handlers/users.php';
+require __DIR__ . '/../handlers/answers.php';
+require __DIR__ . '/../handlers/questions.php';
+require __DIR__ . '/../handlers/subjects.php';
+require __DIR__ . '/../handlers/authentication.php';
 
 $app->run();
