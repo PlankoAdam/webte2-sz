80f05c389a6ce681793c78e18262f21b58165877
Adam Planko <adi.planko@gmail.com>
2024-05-14 16:01:32 +0200

    result percents

--- frontend/src/components/MultiChoiceResults.vue
+++ frontend/src/components/MultiChoiceResults.vue
@@ -4,14 +4,14 @@
       v-for="ans in sortedAnswers"
       :key="ans"
       class="p-2 ps-4 rounded-md flex flex-row justify-between min-w-96 max-w-[32rem] bg-gradient-to-r from-[var(--clr),var(--perc)] via-[var(--clr),var(--perc)] to-[var(--color-bg-soft)]"
-      :style="`--perc:${ans.percent}%; --clr:${ans.is_correct ? 'var(--color-good)' : 'var(--color-bg-mute)'};`"
+      :style="`--perc:${ans.fill_percent}%; --clr:${ans.is_correct ? 'var(--color-good)' : 'var(--color-bg-mute)'};`"
       :class="{ 'text-white': ans.is_correct }"
     >
       <p class="overflow-hidden text-ellipsis whitespace-nowrap me-4 text-lg">
         {{ ans.answer }}
       </p>
-      <p class="bg-[var(--color-bg)] text-[var(--color-text)] px-3 rounded-md font-bold">
-        {{ ans.count }}
+      <p class="bg-[var(--color-bg)] text-[var(--color-text)] px-3 rounded-md min-w-fit">
+        {{ `${ans.percent}% (${ans.count})` }}
       </p>
     </div>
   </div>
@@ -25,12 +25,15 @@ const props = defineProps({
 })
 
 const max = ref(0)
+const total = ref(0)
 const sortedAnswers = computed(() => {
   calcMax()
+  calcTotal()
   let res = props.answers ? props.answers : []
   res.map((a) => {
     let ret = a
-    ret.percent = max.value == 0 ? 0 : Math.round((ret.count / max.value) * 100)
+    ret.fill_percent = max.value == 0 ? 0 : Math.round((ret.count / max.value) * 100)
+    ret.percent = total.value == 0 ? 0 : Math.round((ret.count / total.value) * 100)
     return ret
   })
   console.log(res)
@@ -47,5 +50,11 @@ const calcMax = () => {
   })
 }
 
-console.log(sortedAnswers.value)
+const calcTotal = () => {
+  if (!props.answers) return
+  total.value = 0
+  props.answers.forEach((a) => {
+    total.value += a.count
+  })
+}
 </script>
